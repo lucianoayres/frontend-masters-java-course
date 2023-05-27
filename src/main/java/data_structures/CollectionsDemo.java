698a164442a309ab9ab999da23738b014c7475ed
Luciano Ayres <lucianofarias@gmail.com>
2023-05-27 12:57:17 -0300

    feat: add data_structures and streams exercises

--- src/main/java/data_structures/CollectionsDemo.java
+++ src/main/java/data_structures/CollectionsDemo.java
@@ -6,8 +6,64 @@ public class CollectionsDemo {
 
     public static void main(String[] args) {
         //setDemo();
-        //listDemo();
-        queueDemo();
+        listDemo();
+        //queueDemo();
+        //mapDemo();
+
+    }
+
+    private static void mapDemo() {
+        Map<String, Integer> fruitCalories = new HashMap(); // unordered key-value list
+        fruitCalories.put("apple", 95);
+        fruitCalories.put("lemon", 20);
+        fruitCalories.put("banana", 105);
+        fruitCalories.put("orange", 45);
+
+        fruitCalories.put("orange", 45); // if you add a repeated key, it will replace the previous one
+
+        fruitCalories.putIfAbsent("lemon", 17); // only adds if there`s no identical key
+
+        fruitCalories.remove("banana");
+
+        System.out.println(fruitCalories);
+        System.out.println("apple calories: " + fruitCalories.get("apple"));
+        System.out.println("contains orange? " + fruitCalories.containsKey("orange"));
+
+        Map immutableFruitCalories = Map.of(1, "grapes", 2, "strawberry");
+
+        System.out.println(immutableFruitCalories);
+
+        Set mapEntries = fruitCalories.entrySet();
+        var i = mapEntries.iterator();
+
+        while(i.hasNext()){
+            Map.Entry entry = (Map.Entry)i.next();
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+
+        System.out.println("Enhanced For loop");
+
+        for (Map.Entry calorieInfo : fruitCalories.entrySet()) {
+            System.out.println(calorieInfo.getKey() + ": " +  calorieInfo.getValue());
+        }
+
+        Map<String, String> newFruitMap = new HashMap();
+
+        newFruitMap.put("apple", "good");
+        newFruitMap.put("lemon", "average");
+        newFruitMap.put("watermelon", "great");
+
+
+        for (Map.Entry newFruit : newFruitMap.entrySet()) {
+            System.out.println(newFruit.getKey() + ": " +  newFruit.getValue());
+        }
+
+        // Iterating with forEach
+
+        System.out.println("Iterating with forEach");
+
+        fruitCalories.forEach((k,v) -> System.out.println(k + ": " + v));
 
     }
 
@@ -28,7 +84,7 @@ public class CollectionsDemo {
     }
 
     private static void listDemo() {
-        List fruits = new ArrayList();
+        List<String> fruits = new ArrayList();
         fruits.add("apple");
         fruits.add("lemon");
         fruits.add("banana");
@@ -46,6 +102,25 @@ public class CollectionsDemo {
         System.out.println(fruits);
 
         List moreFruit = List.of("cherry", "plum"); //immutable
+
+        // Iterating with enhanced For loop
+        System.out.println("Iterating with enhanced For loop");
+        for(String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+        System.out.println("Iterating with forEach");
+
+        fruits.forEach(f-> System.out.println(f)); // lambda expression as parameter to print each item
+
+        System.out.println("Using method reference");
+        // method reference is just syntax sugar for a lambda expression
+        fruits.forEach(System.out::println); // lambda expression with `method reference`(classname)
+
+        fruits.forEach(f-> {
+            System.out.println("Lambda function (anonymous function) with code block");
+            System.out.println(f);
+        }); // with multiple lines in the expression
     }
 
     public static void setDemo(){
@@ -68,7 +143,7 @@ public class CollectionsDemo {
         Set moreFruit = Set.of("pear", "raisin", "cherry");
 
         // this will throw an error, because the usage of 'Set.of` makes the Set immutable
-        moreFruit.add("cranberry");
+        // moreFruit.add("cranberry");
 
         System.out.println(moreFruit);
 
@@ -80,5 +155,12 @@ public class CollectionsDemo {
         numbers.add(secondNumber);
 
         System.out.println(numbers);
+
+        // loop Set with iterator
+        System.out.println("Loop Set item with iterator");
+        var i = fruits.iterator();
+        while(i.hasNext()) {
+            System.out.println(i.next());
+        };
     }
 }
---
+++ src/main/java/streams/StreamOperations.java
@@ -0,0 +1,58 @@
+package streams;
+
+import java.util.List;
+
+public class StreamOperations {
+
+    static List<String> veggies = List.of(
+            "spinach",
+            "cabbage",
+            "peas",
+            "green beans",
+            "brussel sprouts",
+            "carrots"
+    );
+
+    public static void main(String[] args) {
+        //anyMatchDemo();
+        //allMatchDemo();
+        //filterDemo();
+        //mapDemo();
+        //reduceDemo();
+        intReduceDemo();
+    }
+
+    private static void intReduceDemo() {
+        List<Integer> numbers = List.of(2, 4, 6, 8, 10);
+
+        var sum = numbers.stream().reduce(0, (a,b) -> a + b);
+        System.out.println(sum);
+    }
+
+    private static void reduceDemo() {
+        // sorts the list of strins in alphabetica order, then join them separated with pipe
+        System.out.println(veggies.stream().sorted().reduce("",(a,b) -> a + " | " + b));
+    }
+
+    public static void anyMatchDemo() {
+        boolean anyMatches = veggies.stream().anyMatch(v -> v.contains(" "));
+        System.out.println(anyMatches);
+    }
+
+    public static void allMatchDemo(){
+        boolean anyMatches = veggies.stream().allMatch(v -> v.contains("s"));
+        System.out.println(anyMatches);
+    }
+
+    public static void filterDemo() {
+        veggies.stream()
+                .filter(v -> v.startsWith(("c")))
+                .forEach(System.out::println);
+    }
+
+    public static void mapDemo(){
+        veggies.stream()
+                .map(String::toUpperCase)
+                .forEach(System.out::println); // the parameter could be replaced for v -> v.toUpperCase()
+    }
+}
